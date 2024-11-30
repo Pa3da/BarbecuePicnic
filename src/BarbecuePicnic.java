d76ba39006b1fc7e5938be4885668e1006da58fd
Matinal <2982347103@qq.com>
2024-11-30 20:55:15 +0800

    zhy修改了printFormat，并且修改了int[]，变成java样式了

--- src/BarbecuePicnic.java
+++ src/BarbecuePicnic.java
@@ -1,37 +1,44 @@
 
 
 class Base {
-	public static int NumberOfSkewers[]= new int[3];
-	
+    public static int[] NumberOfSkewers = new int[3];
+
 }
 
 class GrillMaster extends Base {
-	public GrillMaster() {
-		NumberOfSkewers[1] = 2;
-	}
+    public GrillMaster() {
+        NumberOfSkewers[1] = 2;
+    }
 }
 
 class Customer extends Base {
-	public Customer() {
-		NumberOfSkewers[1] = 1;
-	}
+    public Customer() {
+        NumberOfSkewers[1] = 1;
+    }
 }
 
 
 public class BarbecuePicnic {
-	public static void main(String[] args) {
-		System.out.println(Base.NumberOfSkewers);
-		System.out.println(Customer.NumberOfSkewers);
-		System.out.println(GrillMaster.NumberOfSkewers);
-		new Customer();
-		System.out.println(Base.NumberOfSkewers);
-		System.out.println(Customer.NumberOfSkewers);
-		System.out.println(GrillMaster.NumberOfSkewers);
-		new GrillMaster();
-		System.out.println(Base.NumberOfSkewers);
-		System.out.println(Customer.NumberOfSkewers);
-		System.out.println(GrillMaster.NumberOfSkewers);
-	}
-	
-	
+    public static void main(String[] args) {
+        printFormat(Base.NumberOfSkewers);
+        printFormat(Customer.NumberOfSkewers);
+        printFormat(GrillMaster.NumberOfSkewers);
+        System.out.println("new Customer");
+        new Customer();
+        printFormat(Base.NumberOfSkewers);
+        printFormat(Customer.NumberOfSkewers);
+        printFormat(GrillMaster.NumberOfSkewers);
+        System.out.println("new GrillMaster");
+        new GrillMaster();
+        printFormat(Base.NumberOfSkewers);
+        printFormat(Customer.NumberOfSkewers);
+        printFormat(GrillMaster.NumberOfSkewers);
+    }
+
+
+    public static void printFormat(int[] a) {
+        for (int num : a) {
+            System.out.println(num);
+        }
+    }
 }
