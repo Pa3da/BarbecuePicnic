34b30178e243eea92f49f31356f00c7fc00389fe
zhypzq <p3316049519@163.com>
2024-11-30 22:21:57 +0800

    在base类里面写了id和name，并且尝试了创建了一下顾客和厨师的线程对象并把base类改成了抽象类，并且修改了两个子类的构造函数。

--- src/BarbecueRestaurant.java
+++ src/BarbecueRestaurant.java
@@ -1,7 +1,9 @@
 /**
  * 父类
  */
-class Base {
+abstract class Base implements Runnable {
+	public int id;
+	public String name;
 	//餐厅桌子数量
 	public static int[] NumberOfTables = new int[Constants.NUMBER_OF_TABLES];
 	
@@ -12,8 +14,14 @@ class Base {
  * 烧烤师傅
  */
 class GrillMaster extends Base {
-	public GrillMaster() {
-		
+	public GrillMaster(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("GrillMaster " + this.id + "号 " + this.name);
 	}
 }
 
@@ -21,8 +29,14 @@ class GrillMaster extends Base {
  * 顾客
  */
 class Customer extends Base {
-	public Customer() {
-		
+	public Customer(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Customer " + this.id + "号 " + this.name);
 	}
 }
 
@@ -49,7 +63,7 @@ public class BarbecueRestaurant {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		init();
 	}
 	
 	
@@ -62,7 +76,10 @@ public class BarbecueRestaurant {
 	/**
 	 * 初始化
 	 */
-	public void init() {
-		
+	public static void init() {
+		Thread grillMaster1 = new Thread(new GrillMaster("Matinal", 1));
+		Thread customer1 = new Thread(new Customer("Matinal", 2));
+		grillMaster1.start();
+		customer1.start();
 	}
 }
